530cde4d1f8d848b0fb93679d96dbd84591c46ae
KarolPiasnik <piasnik.karol@outlook.com>
2023-08-28 20:08:03 +0200

    do not respond to bot's messages

--- src/main/kotlin/org/piasnik/DiscordClient.kt
+++ src/main/kotlin/org/piasnik/DiscordClient.kt
@@ -3,18 +3,19 @@ package org.piasnik
 import discord4j.core.DiscordClient
 import discord4j.core.event.domain.message.MessageCreateEvent
 
-
 object DiscordClient {
     @JvmStatic
     fun main(args: Array<String>) {
         val client = DiscordClient.create("yourDiscordToken")
         val gateway = client.login().block() ?: return
 
-        gateway.on(MessageCreateEvent::class.java).subscribe { event ->
-            println(event.message)
-            val input = event.message
-            client.getChannelById(input.channelId).createMessage("Otrzymałem twoją wiadomość :) :${input.content}")
-                .block()
+        gateway.on(MessageCreateEvent::class.java).subscribe { event: MessageCreateEvent ->
+            val message = event.message
+            if (!message.author.get().isBot) {
+                val channel = message.channel.block()
+                channel.createMessage("Otrzymałem twoją wiadomość ${message.author.get().username} :) ")
+                    .block()
+            }
         }
 
         gateway.onDisconnect().block()
